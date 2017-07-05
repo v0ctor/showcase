846952680e94f968a91b970bf1e34cd33e702990
Víctor Díaz Marco <victor@victordiaz.me>
2017-07-05 12:12:30 +0200

    Old routes redirected permanently.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,16 @@ Route::get('', function () {
 });
 
 # Publications
+
+// WebSocket
+Route::get('/2015/11/15/coneixent-websocket', function () {
+	return redirect('/websocket?hl=ca', 301);
+});
+
+Route::get('/2015/11/15/conociendo-websocket', function () {
+	return redirect('/websocket?hl=es', 301);
+});
+
 Route::get('websocket', function () {
 	if (App::getLocale() === 'en') {
 		setlocale(LC_TIME, SetLanguage::SYSTEM_LOCALES['es']);
@@ -22,6 +32,15 @@ Route::get('websocket', function () {
 	}
 	
 	return view('websocket');
+});
+
+// Bitcoin
+Route::get('/2016/04/13/bitcoin-plantejament-i-protocol', function () {
+	return redirect('/bitcoin?hl=ca', 301);
+});
+
+Route::get('/2016/04/13/bitcoin-planteamiento-y-protocolo', function () {
+	return redirect('/bitcoin?hl=es', 301);
 });
 
 Route::get('bitcoin', function () {
